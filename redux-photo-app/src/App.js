0448f4a974d3bba949eb1afb5da5ee5f29bd6515
tuongnc <s3818196@rmit.edu.vn>
2021-07-12 17:09:45 +0700

    ReactJs

--- redux-photo-app/src/App.js
+++ redux-photo-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter, Route, Switch, Redirect, Link } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './App.scss';
+import Header from './components/Header';
 import NotFound from './components/NotFound';
 
 // Lazy load - Code splitting
@@ -11,18 +12,12 @@ function App() {
     <div className="photo-app">
       <Suspense fallback={<div>Loading ...</div>}>
         <BrowserRouter>
-
-          {/* TODO: Remove after testing */}
-          <ul>
-            <li><Link to="/photo">Go to photo page</Link></li>
-            <li><Link to="/photo/add">Go to Add new photo page</Link></li>
-            <li><Link to="/photo/123">Go to Edit photo page</Link></li>
-          </ul>
+          <Header />
 
           <Switch>
-            <Redirect exact from="/" to="/photo" />
+            <Redirect exact from="/" to="/photos" />
 
-            <Route path="/photo" component={Photo} />
+            <Route path="/photos" component={Photo} />
             <Route component={NotFound} />
           </Switch>
         </BrowserRouter>
