23c0c407d030625d2703947592802510cba9dd35
tuongnc <s3818196@rmit.edu.vn>
2021-07-13 17:00:55 +0700

    add to list

--- redux-photo-app/src/features/Photo/pages/Main/index.jsx
+++ redux-photo-app/src/features/Photo/pages/Main/index.jsx
@@ -1,12 +1,15 @@
 import Banner from 'components/Banner';
 import Images from 'constants/images';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Container } from 'reactstrap';
 
 MainPage.propTypes = {};
 
 function MainPage(props) {
+  const photos = useSelector(state => state.photos);
+  console.log(photos);
   return (
     <div className="photo-main">
       <Banner title="Your awesome photos 🎉" backgroundUrl={Images.PINK_BG} />
--- redux-photo-app/src/index.js
+++ redux-photo-app/src/index.js
@@ -5,10 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from 'app/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
